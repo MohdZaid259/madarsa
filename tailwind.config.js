d3803b21cbc59679a50884ffda31e4e79250217c
Mohd Zaid <[razvizaid259@gmail.com]>
2025-05-29 11:08:24 +0530

    fixes tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
     "*.{js,ts,jsx,tsx,mdx}",
-    "./index.html", "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
